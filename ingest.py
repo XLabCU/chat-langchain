b2d37b768a7b83aa641064a45e0f3a9fc196d912
Shawn Graham <shawn.graham@carleton.ca>
2023-03-18 20:49:40 -0400

    Add files via upload

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
 import pickle
 
-from langchain.document_loaders import ReadTheDocsLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
@@ -9,8 +9,10 @@ from langchain.vectorstores.faiss import FAISS
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    loader = DirectoryLoader('./traffickingculture.org/', glob="**/**/**/*.html")
+
     raw_documents = loader.load()
+    print(raw_documents)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
